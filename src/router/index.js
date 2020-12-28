57b8878ab5637764143504220e32b083c872b542
XdongM <2061977493@qq.com>
2020-12-28 17:19:26 +0800

    1

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../components/HelloWorld.vue'
+import Login from '../components/login.vue'
+import Main from '../components/main.vue'
+import Information from '../components/information.vue'
+import Money from '../components/money.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/',
+    name: 'Main',
+    component: Main
+  },
+  {
+    path: '/information',
+    name: 'Information',
+    component: Information
+  },
+  {
+    path: '/money',
+    name: 'money',
+    component: Money
+  },
+  {
+    path: '/hello',
+    name: 'hello',
+    component: Home
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
